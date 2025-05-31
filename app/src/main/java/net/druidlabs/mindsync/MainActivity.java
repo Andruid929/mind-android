19ab2932567e88766742d07a2056c48ba1c82076
Andruid929 <princeajsa@gmail.com>
2025-05-31 17:36:04 +0200

    0.1.0 - Added Note.java and Notes.java for control over notes - Added launcher icon mipmap - Changed primaryColour resources for better matching wiht desired app   theme

--- app/src/main/java/net/druidlabs/mindsync/MainActivity.java
+++ app/src/main/java/net/druidlabs/mindsync/MainActivity.java
@@ -1,14 +1,48 @@
 package net.druidlabs.mindsync;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+import net.druidlabs.mindsync.notes.Note;
+import net.druidlabs.mindsync.notes.Notes;
+
+import java.util.List;
+
+/**
+ * The activity you boot into when you launch the application.
+ * This activity is what starts off every single project unless opened by
+ * an external application.
+ *
+ * @since 0.0.1
+ * @version 1.0
+ * @author Andrew Jones
+ * */
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout homeDrawerLayout;
+
+    private NavigationView homeNavigationView;
+
+    private MaterialToolbar homeToolbar;
+
+    private GridView notesListGridView;
+
+    private FloatingActionButton expandAddFab;
+    private FloatingActionButton addNoteFab;
+
+    private List<Note> notesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +54,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        homeDrawerLayout = findViewById(R.id.main);
+
+        homeNavigationView = findViewById(R.id.home_drawer_nav_view);
+
+        homeToolbar = findViewById(R.id.home_toolbar);
+
+        notesListGridView = findViewById(R.id.home_notes_gridview);
+
+        expandAddFab = findViewById(R.id.home_expand_add_fab);
+        addNoteFab = findViewById(R.id.home_add_note_fab);
+
+        notesList = Notes.getNotes();
     }
 }
---
+++ app/src/main/java/net/druidlabs/mindsync/notes/Note.java
@@ -0,0 +1,85 @@
+package net.druidlabs.mindsync.notes;
+
+import java.util.Objects;
+
+/**
+ * This class represents a note and contains the note's data.
+ * <p>The moment an object of this clas and the parameters are valid,
+ * this note is automatically added to {@code Notes.notes}.
+ *
+ * @author Andrew Jones
+ * @version 1.0
+ * @since 0.0.1
+ */
+
+public final class Note {
+
+    public static final String TEST_HEADING = "Testing header";
+    public static final String TEST_BODY = "Testing body";
+
+    /**
+     * The note's heading.
+     */
+
+    private final String heading;
+
+    /**
+     * The note's body
+     */
+
+    private final String body;
+
+    /**
+     * Get a new note instance.
+     *
+     * @param heading the heading/title of the note.
+     * @param body    the body of the note.
+     */
+
+    public Note(String heading, String body) {
+        this.heading = heading;
+        this.body = body;
+
+        if (heading.isBlank()) {
+            return;
+        }
+
+        if (heading.equals(TEST_HEADING) && body.equals(TEST_BODY)) {
+            return;
+        }
+
+        Notes.notes.add(this);
+    }
+
+    /**
+     * Get the name of this note.
+     *
+     * @return the heading of the current note.
+     */
+
+    public String getHeading() {
+        return heading;
+    }
+
+    /**
+     * Get the body.
+     *
+     * @return the body of this note.
+     */
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return Objects.equals(heading, note.heading) && Objects.equals(body, note.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(heading, body);
+    }
+}
