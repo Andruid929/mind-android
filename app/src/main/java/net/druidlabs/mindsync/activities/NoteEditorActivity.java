857571848fce79efa142f8942ac57fbaaffd1faa
Andruid929 <princeajsa@gmail.com>
2025-06-08 08:15:32 +0200

    0.7.0 - Added back button to NoteEditorActivity for accessibility - Changed editor_note_body_edittext layout height to 0dp to fill the   space. - Added note info bar at the bottom of the Note editor activity - New string resources - Added helper method getStringResouce in NoteEditorActivity

--- app/src/main/java/net/druidlabs/mindsync/activities/NoteEditorActivity.java
+++ app/src/main/java/net/druidlabs/mindsync/activities/NoteEditorActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 import net.druidlabs.mindsync.R;
 import net.druidlabs.mindsync.notes.Note;
@@ -28,6 +31,11 @@ public class NoteEditorActivity extends AppCompatActivity {
 
     private Note currentNote;
 
+    private MaterialButton goBackBtn;
+
+    private TextView bodyCharCountTextView;
+    private TextView noteCreationTimeTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +50,13 @@ public class NoteEditorActivity extends AppCompatActivity {
         EditText noteHeadingEditText = findViewById(R.id.editor_heading_edittext);
         EditText noteBodyEditText = findViewById(R.id.editor_body_edittext);
 
+        goBackBtn = findViewById(R.id.editor_toolbar_back_btn);
+
+        bodyCharCountTextView = findViewById(R.id.editor_note_character_num_textview);
+        noteCreationTimeTextView = findViewById(R.id.editor_note_time_created_textview);
+
+        goBackBtn.setOnClickListener(v -> finish());
+
         //The clicked note's index
         int currentNoteIndex = getIntent().getIntExtra(Note.INTENT_NOTE_POSITION, -1);
 
@@ -49,6 +64,18 @@ public class NoteEditorActivity extends AppCompatActivity {
 
         String noteHeading = currentNote.getHeading();
         String noteBody = currentNote.getBody();
+
+        int numOfCharsInBody = noteBody.length();
+
+        String charText;
+
+        if (numOfCharsInBody == 1) {
+            charText = "1 " + getStringResource(R.string.editor_note_info_character);
+        } else {
+            charText = numOfCharsInBody + " " + getStringResource(R.string.editor_note_info_character_num);
+        }
+
+        bodyCharCountTextView.setText(charText);
 
         noteHeadingEditText.setText(noteHeading);
         noteBodyEditText.setText(noteBody);
@@ -81,11 +108,35 @@ public class NoteEditorActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 currentNote.setBody(s.toString());
+
+                String charText;
+
+                int numOfCharsInBody = s.length();
+
+                if (numOfCharsInBody == 1) {
+                    charText = "1 " + getStringResource(R.string.editor_note_info_character);
+                } else {
+                    charText = numOfCharsInBody + " " + getStringResource(R.string.editor_note_info_character_num);
+                }
+
+                bodyCharCountTextView.setText(charText);
             }
 
             @Override
             public void afterTextChanged(Editable s) {
             }
         });
+    }
+
+    /**
+     * Get the string resource from the specific resource ID.
+     *
+     * @param resId the string resource ID.
+     * @return the String represented by the {@code resID}.
+     * @since 0.7.0
+     * */
+
+    private String getStringResource(int resId) {
+        return getResources().getString(resId);
     }
 }
